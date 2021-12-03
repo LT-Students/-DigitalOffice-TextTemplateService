526afcfe8eadf9783587e59ae59ce372a38124c1
Ekaterina <89151831+grinakate@users.noreply.github.com>
2021-12-03 18:15:05 +0300

    add TextTemplateService (#1)
    * add TextTemplateService
    
    * fix
    
    * fix
    
    * fix endpoints
    
    * fix tabs
    
    * fix
    
    * fix
    
    * fix

---
+++ src/TextTemplateService.Data.Provider.MsSql.Ef/TextTemplateServiceDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace LT.DigitalOffice.TextTemplateService.Data.Provider.MsSql.Ef
+{
+  public class TextTemplateServiceDbContext : DbContext, IDataProvider
+  {
+    public TextTemplateServiceDbContext(DbContextOptions<TextTemplateServiceDbContext> options)
+    : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.ApplyConfigurationsFromAssembly(Assembly.Load("LT.DigitalOffice.TextTemplateService.Models.Db"));
+    }
+
+    public void EnsureDeleted()
+    {
+      Database.EnsureDeleted();
+    }
+
+    public bool IsInMemory()
+    {
+      return Database.IsInMemory();
+    }
+
+    public object MakeEntityDetached(object obj)
+    {
+      Entry(obj).State = EntityState.Detached;
+
+      return Entry(obj).State;
+    }
+
+    public void Save()
+    {
+      SaveChanges();
+    }
+
+    public async Task SaveAsync()
+    {
+      await SaveChangesAsync();
+    }
+  }
+}
---
+++ src/TextTemplateService.Models.Dto/Configuration/RabbitMqConfig.cs
@@ -0,0 +1,17 @@
+﻿using LT.DigitalOffice.Kernel.BrokerSupport.Attributes;
+using LT.DigitalOffice.Kernel.BrokerSupport.Configurations;
+using LT.DigitalOffice.Models.Broker.Common;
+using LT.DigitalOffice.Models.Broker.Requests.Company;
+using LT.DigitalOffice.Models.Broker.Requests.Department;
+using LT.DigitalOffice.Models.Broker.Requests.Image;
+using LT.DigitalOffice.Models.Broker.Requests.Office;
+using LT.DigitalOffice.Models.Broker.Requests.Position;
+using LT.DigitalOffice.Models.Broker.Requests.Project;
+using LT.DigitalOffice.Models.Broker.Requests.User;
+
+namespace LT.DigitalOffice.TextTemplateService.Models.Dto.Configuration
+{
+  public class RabbitMqConfig : BaseRabbitMqConfig
+  {
+  }
+}
