60608a7aecaa24045d695f0449b67698124e4f40
sparrowofnight@gmail.com <sparrowofnight@gmail.com>
2022-11-28 17:39:34 +0300

    tab

--- src/TextTemplateService.Data.Provider.MsSql.Ef/Migrations/20221026220000_UpdateUrlInTemplates.cs
+++ src/TextTemplateService.Data.Provider.MsSql.Ef/Migrations/20221026220000_UpdateUrlInTemplates.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 
+
 namespace LT.DigitalOffice.TextTemplateService.Data.Provider.MsSql.Ef.Migrations;
 
 [Migration("20221026220000_UpdateUrlInTemplates")]
